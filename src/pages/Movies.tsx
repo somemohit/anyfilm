ab7cc2892fcedacf14a6b8aaff71154833d0bfbc
somemohit <98733767+somemohit@users.noreply.github.com>
2025-01-28 22:04:05 +0530

    navbar & homepage api intg

--- src/pages/Movies.tsx
+++ src/pages/Movies.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Movies = () => {
   return (
-    <div>Moviess</div>
+    <div>Movies</div>
   )
 }
 
--- src/pages/TvShows.tsx
+++ src/pages/TvShows.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const TvShows = () => {
   return (
-    <div>TvShowss</div>
+    <div>TvShows</div>
   )
 }
 
