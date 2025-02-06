0be45fca9fe9a9a826e8bfdc6bf938f03331f7ba
somemohit <98733767+somemohit@users.noreply.github.com>
2025-02-06 14:54:04 +0530

    bug fixes

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -8,7 +8,7 @@ const Loader = () => {
           visible={true}
           height="80"
           width="80"
-          color="#4fa94d"
+          color="#fb2c36"
           ariaLabel="triangle-loading"
           wrapperStyle={{}}
           wrapperClass=""
